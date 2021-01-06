e08a354cd5fdf1b94a5d637224942b1cd28c2bb0
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-06 09:24:25 -0300

    Create user model and create mongoose schema

---
+++ src/model/user.ts
@@ -0,0 +1,26 @@
+import mongoose, { Model } from "mongoose";
+import { IUserModel } from "./interfaces/IUserModel";
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export const User: Model<IUserModel> = mongoose.model("User", schema);
