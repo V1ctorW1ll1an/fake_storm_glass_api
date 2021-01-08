ad3f0aa4b170f8da92815d1c351da999582ec1ff
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-01-07 22:38:05 -0300

    Verify if email already exists

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Model } from "mongoose";
+import { CUSTOM_VALIDATION } from "./enums/custom_validation";
 import { IUserModel } from "./interfaces/IUserModel";
 
 const schema = new mongoose.Schema(
@@ -21,6 +22,15 @@ const schema = new mongoose.Schema(
       },
     },
   }
+);
+
+schema.path("email").validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  "already exists in the database.",
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<IUserModel> = mongoose.model("User", schema);
