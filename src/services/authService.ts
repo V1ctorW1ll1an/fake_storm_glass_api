ecc3b808c7dee6fd1fba65e26380d05013ffcd6d
Victor Willian <victorwillian1012@gmail.com>
2021-01-11 16:46:35 -0300

    Add token service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,7 @@
+import { IUserModel } from "@src/model/interfaces/IUserModel";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "config";
 
 export class AuthService {
   public static async hashPassword(
@@ -13,5 +16,12 @@ export class AuthService {
     hashedPassword: string
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static generateToken(payload: IUserModel): string {
+    const payloadJson = payload.toJSON();
+    return jwt.sign(payloadJson, config.get("App.auth.key"), {
+      expiresIn: config.get("App.auth.tokenExpiresIn"),
+    });
   }
 }
